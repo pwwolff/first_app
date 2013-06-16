370ed757f7a7c228f1d6c5d5dd49d7814ee7dbe2
pwwolff <pwwolff@gmail.com>
2013-06-16 04:27:48 +0200

    FirsifokbIkbkbdkbFkbfkbFirtkbst Commkbkbkbkbcommitkb;First kbkbkbkbkbkbThkbkbFirst kbkbkbkbkbkbFirst FkbGkbtime usinkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbkbL:w":""kdkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkdklklklklklklklklklklklklklklklklkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrkrgikb:kb:kblkb;	kb:gikbkbqLkbkbkbjjkljklllllllllllllhhhhhhh:q!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a5c70819baa655eb2e173c62a00012b4fc3b5b7d6673225803fbf671a6b07c0a07aacb09ee6489178ecf8527c06597c8b09eef0c52e401c1b1d5b4c1551cc4c6'
